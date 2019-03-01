548192ddd086eeef29537a96156423612cdabf00
SerenaB91 <serenabarakat2@gmail.com>
2019-03-01 16:45:04 +0100

    Fixed minor issues and added menu functionality Fixed code duplication issue Added first version menu Changed Hibernate query from create to update in database

--- src/main/java/se/kth/sda5/serena/hibernate/util/HibernateQuery.java
+++ src/main/java/se/kth/sda5/serena/hibernate/util/HibernateQuery.java
@@ -1,69 +1,51 @@
 package se.kth.sda5.serena.hibernate.util;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import se.kth.sda5.serena.dto.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HibernateQuery {
 
-    public static void addUser(User user){
+    public static void addObject(Class className, Object object){
+
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         Transaction transaction = null;
         Integer id = null;
         transaction = session.beginTransaction();
-        id = (Integer) session.save(user);
+        if(className.getName().equals("se.kth.sda5.serena.dto.Task")){
+            Task task = (Task) object;
+            id = (Integer) session.save(task);
+        }else if(className.getName().equals("se.kth.sda5.serena.dto.Subtask")){
+            Subtask subtask = (Subtask) object;
+            id = (Integer) session.save(subtask);
+        }
+        else if(className.getName().equals("se.kth.sda5.serena.dto.Member")){
+            Member member = (Member) object;
+            id = (Integer) session.save(member);
+        }
+        else if(className.getName().equals("se.kth.sda5.serena.dto.Status")){
+            Status status = (Status) object;
+            id = (Integer) session.save(status);
+        }
+        else if(className.getName().equals("se.kth.sda5.serena.dto.User")){
+            User user = (User) object;
+            id = (Integer) session.save(user);
+        }
+        else if(className.getName().equals("se.kth.sda5.serena.dto.Project")){
+            Project project = (Project) object;
+            id = (Integer) session.save(project);
+        }
         transaction.commit();
         session.close();
     }
 
-    public static void addProject(Project project){
+    public static List getAllData(Class c) {
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        Transaction transaction = null;
-        Integer id = null;
-        transaction = session.beginTransaction();
-        id = (Integer) session.save(project);
-        transaction.commit();
-        session.close();
+        List list = session.createCriteria(c).list();
+        return list;
     }
-
-    public static void addStatus(Status status){
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        Transaction transaction = null;
-        Integer id = null;
-        transaction = session.beginTransaction();
-        id = (Integer) session.save(status);
-        transaction.commit();
-        session.close();
-    }
-
-    public static void addMember(Member member){
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        Transaction transaction = null;
-        Integer id = null;
-        transaction = session.beginTransaction();
-        id = (Integer) session.save(member);
-        transaction.commit();
-        session.close();
-    }
-
-    public static void addTask(Task task){
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        Transaction transaction = null;
-        Integer id = null;
-        transaction = session.beginTransaction();
-        id = (Integer) session.save(task);
-        transaction.commit();
-        session.close();
-    }
-
-    public static void addSubtask(Subtask subtask){
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        Transaction transaction = null;
-        Integer id = null;
-        transaction = session.beginTransaction();
-        id = (Integer) session.save(subtask);
-        transaction.commit();
-        session.close();
-    }
-
 }
